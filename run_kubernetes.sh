9c09ef1929f20de2f237a7d9b6176646d0f103a6
DungTNT <dungtnt.dev@gmail.com>
2022-07-16 14:41:32 +0700

    complete task 6

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,23 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=dungtnt2244/ml-app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+r=$(kubectl get pod ml-app 2> /dev/null;echo $?)
 
+if [ "_$r" == "_1" ] ; then
+    kubectl run ml-app \
+    --image=$dockerpath \
+    --image-pull-policy="Always" \
+    --overrides='{"apiVersion": "v1", "spec":{"imagePullSecrets": [{"name": "regcred"}]}}'
+fi
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward ml-app 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=dungtnt2244/ml-app
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
+docker login -u dungtnt2244
+docker tag ml-app $dockerpath:latest
+
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
